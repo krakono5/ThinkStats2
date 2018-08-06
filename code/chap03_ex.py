303df3744d5eb2eea7f44cb66c4984670c63a08a
Markus <mgsinn@gmail.com>
2018-08-06 21:52:25 +0200

    added file with basic main function for solution from chapter 3

---
+++ code/chap03_ex.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import numpy as np
+import sys
+
+import nsfg
+import first
+import thinkstats2
+import thinkplot
+
+
+def pmf_mean(pmf):
+    # todo: implement
+    pass
+
+
+def pmf_var(pmf):
+    # todo: implement
+    pass
+
+
+def main():
+    print('START MAIN')
+
+    data = nsfg.ReadFemResp()
+    # todo: look up structure of pmf, write functions and compare to 'original'
+
+
+    print('END MAIN')
+
+
+if __name__ == '__main__':
+    main()
